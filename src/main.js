799e92b8136a78760868064f74d525e830353a13
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-03-08 13:19:57 +0100

    test

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { addMarkup } from './js/render-functions.js';
 import { searchImages } from './js/pixabay-api.js';
-import { loadMore } from './js/pixabay-api.js';
 
 const options = {
   captionsData: 'alt',
@@ -25,9 +24,10 @@ const loader = document.querySelector('.loader');
 const loadMoreBtn = document.querySelector('.load-more-btn');
 
 let currentPage = 1;
-let loadedItems = 15;
+let itemsPerPage = 15;
 let currentQuery;
 let totalPages;
+let cardHeight;
 
 form.addEventListener('submit', handleFormSubmit);
 
@@ -44,14 +44,15 @@ function handleFormSubmit(event) {
     return;
   }
 
-  searchImages(QUERY)
+  searchImages(QUERY, currentPage, itemsPerPage)
     .then(data => {
       currentQuery = QUERY;
-      totalPages = Math.ceil(data.totalHits / loadedItems);
+      totalPages = Math.ceil(data.totalHits / itemsPerPage);
       addMarkup(data);
       lightbox.refresh();
       form.reset();
-      loadMoreBtn.style.display = 'block';
+      loader.style.display = 'none';
+      loadMoreBtn.style.display = 'none';
     })
     .catch(error => {
       iziToast.error({
@@ -62,14 +63,17 @@ function handleFormSubmit(event) {
         position: 'topRight',
         iconUrl: cross,
       });
+    })
+    .finally(() => {
+      loadMoreBtn.style.display = 'block';
     });
 }
 
 loadMoreBtn.addEventListener('click', handleLoadMore);
 
-function handleLoadMore() {
-  currentPage += 1;
+async function handleLoadMore() {
   loader.style.display = 'block';
+  currentPage += 1;
 
   if (currentPage === totalPages) {
     loadMoreBtn.style.display = 'none';
@@ -79,24 +83,33 @@ function handleLoadMore() {
     });
   }
 
-  loadMore()
-    .then(data => {
-      console.log(currentPage);
-      console.log(data);
-      gallery.innerHTML += addMarkup(data);
-      lightbox.refresh();
-      loadMoreBtn.style.display = 'block';
-    })
-    .catch(error => {
-      iziToast.error({
-        title: '',
-        message: `MAINError${error}`,
-        backgroundColor: '#EF4040',
-        messageColor: '#ffffff',
-        position: 'topRight',
-        iconUrl: cross,
-      });
+  try {
+    const response = await searchImages(
+      currentQuery,
+      currentPage,
+      itemsPerPage
+    );
+
+    addMarkup(response);
+    smoothScrollBy();
+    lightbox.refresh();
+    loader.style.display = 'none';
+  } catch (error) {
+    iziToast.error({
+      title: '',
+      message: `Error${error}`,
+      backgroundColor: '#EF4040',
+      messageColor: '#ffffff',
+      position: 'topRight',
+      iconUrl: cross,
     });
-  // .finally
-  // loadMoreBtn.style.display = 'none';
+  }
+}
+
+function smoothScrollBy() {
+  cardHeight = gallery.firstElementChild.getBoundingClientRect().height;
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
